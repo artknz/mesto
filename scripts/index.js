d9b39f9b7fdf54a019e4dd44f9bf1cc1cae5e293
Artyom Knyazev <a.knyazev7@yandex.ru>
2020-06-19 00:51:17 +0300

    fix: правки разметки, стилей и js кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,38 @@
 const popup = document.querySelector('.popup')
-const popupOpenButton = document.querySelector('.profile__open-popup')
+const popupOpenButton = document.querySelector('.profile__edit-button_type_open')
 const popupCloseButton = popup.querySelector('.popup__close')
 
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__status');
+let nameInput = document.querySelector('.popup__field_type_name');
+let jobInput = document.querySelector('.popup__field_type_status');
 
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
+let formElement = document.querySelector('.popup__container');
+
 const popupAdd = function () {
   popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent
-  jobInput.value = profileStatus.textContent
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileStatus.textContent;
 }
-
-popupOpenButton.addEventListener('click', popupAdd);
 
 const popupRemove = function () {
   popup.classList.remove('popup_opened');
 }
 
-popupCloseButton.addEventListener('click', popupRemove);
-
-let formElement = document.querySelector('.popup__container');
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    nameInput = nameInput.value
-    jobInput = jobInput.value
+    let nameInputItem = nameInput.value;
+    let jobInputItem = jobInput.value;
 
-    profileName.textContent = nameInput;
-    profileStatus.textContent = jobInput;
+    profileName.textContent = nameInputItem;
+    profileStatus.textContent = jobInputItem;
+
+    popupRemove()
 }
 
+popupOpenButton.addEventListener('click', popupAdd);
+popupCloseButton.addEventListener('click', popupRemove);
+
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', popupRemove);
