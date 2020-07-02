a35dd4849587babf6cc8e37a0be5b5616b048d2d
Artyom Knyazev <a.knyazev7@yandex.ru>
2020-07-02 22:37:37 +0300

    feature: добавление попапа и карточек на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 // Константы и переменные
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close');
+const popupCloseButton = popup.querySelector('.popup__close_type_profile');
+
+const editProfilePopup = document.querySelector('.popup_profile');
+const addCardPopup = document.querySelector('.popup_addcard');
 
 let nameInput = document.querySelector('.popup__field_type_name');
 let jobInput = document.querySelector('.popup__field_type_status');
@@ -9,18 +12,22 @@ let jobInput = document.querySelector('.popup__field_type_status');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup_container_type_edit');
 
 // Обработчик открытия и закрытия попапа и заполнения полей формы
-const popupToggle = function () {
+const popupToggle = function(popup) {
   popup.classList.toggle('popup_opened');
+}
+
+//Добавление текста со старницы в форму
+const addInputText = function() {
   if (popup.classList.contains('popup_opened')){
     nameInput.value = profileName.textContent;
     jobInput.value = profileStatus.textContent;
   }
 }
 
-// Обработчик отправки формы
+// Обработчик отправки формы редактирования профиля
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
@@ -30,14 +37,17 @@ function formSubmitHandler (evt) {
     profileName.textContent = nameInputItem;
     profileStatus.textContent = jobInputItem;
 
-    popupToggle()
+    popupToggle(editProfilePopup);
 }
+
 // Регистраторы событий
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
+popupOpenButton.addEventListener('click', () => popupToggle(editProfilePopup));
+popupOpenButton.addEventListener('click', () => addInputText(editProfilePopup));
+popupCloseButton.addEventListener('click', () => popupToggle(editProfilePopup));
 
 formElement.addEventListener('submit', formSubmitHandler);
 
+//Массив с начальными картинками
 const initialCards = [{
       name: 'Архыз',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -66,8 +76,15 @@ const initialCards = [{
 
 const initialCardsElement = document.querySelector('.elements');
 const addButtonElement = document.querySelector('.profile__add-button');
+const closeButtonCards = document.querySelector('.popup__close_type_cards');
 const cardsTemplateElement = document.querySelector('.elements-template');
 
+let titleInput = document.querySelector('.popup__field_type_title');
+let linkInput = document.querySelector('.popup__field_type_link');
+
+let editForm = document.querySelector('.popup__container_type_add');
+
+//Функция добавления карточки
 function addCard(item) {
   const element = cardsTemplateElement.content.cloneNode(true);
 
@@ -82,4 +99,22 @@ initialCards.forEach(item =>{
   addCard(item);
 });
 
-addButtonElement.addEventListener('click', popupToggle);
+addButtonElement.addEventListener('click', () => popupToggle(addCardPopup));
+closeButtonCards.addEventListener('click', () => popupToggle(addCardPopup));
+editForm.addEventListener('submit', formSubmitCard);
+
+//Обработчик отправки формы добавления карточек
+function formSubmitCard (evt) {
+  evt.preventDefault();
+
+  let titleInputItem = titleInput.value;
+  let linkInputItem = linkInput.value;
+
+  let inputData = {
+    name: titleInputItem,
+    link: linkInputItem
+  };
+
+  addCard(inputData);
+  popupToggle(addCardPopup);
+}
