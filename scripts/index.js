f0f7364435cda8a4e4db61ff3b59cd9250787ec7
Artyom Knyazev <a.knyazev7@yandex.ru>
2020-07-02 15:56:47 +0300

    feature: начальное добавление карточек на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -37,3 +37,49 @@ popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [{
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const initialCardsElement = document.querySelector('.elements');
+const addButtonElement = document.querySelector('.profile__add-button');
+const cardsTemplateElement = document.querySelector('.elements-template');
+
+function addCard(item) {
+  const element = cardsTemplateElement.content.cloneNode(true);
+
+  element.querySelector('.element__image').src = item.link;
+  element.querySelector('.element__text').textContent = item.name;
+  element.querySelector('.element__image').alt = item.name;
+
+  initialCardsElement.prepend(element);
+}
+
+initialCards.forEach(item =>{
+  addCard(item);
+});
+
+addButtonElement.addEventListener('click', popupToggle);
